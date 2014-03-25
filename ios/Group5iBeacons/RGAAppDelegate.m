6c68cac322498da1378c7419afe06cbaae72dec7
John Tubert <jtubert@hotmail.com>
2014-03-25 11:32:59 -0400

    Added FB login screen and functionality

--- ios/Group5iBeacons/RGAAppDelegate.m
+++ ios/Group5iBeacons/RGAAppDelegate.m
@@ -13,14 +13,32 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // TestFlight
-    [TestFlight takeOff:@"<id>"];
+    //[TestFlight takeOff:@"<id>"];
     
-    // Parse
-    [Parse setApplicationId:@"<id>" clientKey:@"<key>"];
-    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
-    [PFFacebookUtils initializeFacebook];
+    // Load the FBProfilePictureView
+    // You can find more information about why you need to add this line of code in our troubleshooting guide
+    // https://developers.facebook.com/docs/ios/troubleshooting#objc
+    [FBProfilePictureView class];
+
+    
     return YES;
 }
+
+// In order to process the response you get from interacting with the Facebook login process,
+// you need to override application:openURL:sourceApplication:annotation:
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+    
+    // You can add your app-specific url handling code here if needed
+    
+    return wasHandled;
+}
+
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
